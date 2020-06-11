3263ae17a7571774d6c06be758ee70f39cda2ea0
saikiran <skiran252@gmail.com>
2020-06-11 21:56:58 +0530

    minimized and added return to callback pattern

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const geocode = require("./utils/geocode")
 const forecast = require("./utils/weatherapi")
 
 
-geocode('Hyderabad',(error,data)=>{
+geocode('Bengaluru',(error,coordinates)=>{
   if (error){
     return console.log(error)
   }
   else {
-  forecast(data.latitude,data.longitude,(error,data)=>{
+  forecast(coordinates.latitude,coordinates.longitude,(error,data)=>{
     if (error){
       return console.log(error)
     }
     else {
+      console.log('Place:',coordinates.location)
       return console.log(data.description,'. It is currently '+data.temp,'C','Todays max: '+data.maxtemp,'and Todays min: '+data.mintemp)
     }
   })
