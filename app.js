99f8b8d43d620db04e719bc6b1e9062fec98ecce
saikiran <skiran252@gmail.com>
2020-06-11 21:59:11 +0530

    removed else in case error return is called and exits

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ geocode('Bengaluru',(error,coordinates)=>{
   if (error){
     return console.log(error)
   }
-  else {
   forecast(coordinates.latitude,coordinates.longitude,(error,data)=>{
+
     if (error){
       return console.log(error)
     }
-    else {
-      console.log('Place:',coordinates.location)
-      return console.log(data.description,'. It is currently '+data.temp,'C','Todays max: '+data.maxtemp,'and Todays min: '+data.mintemp)
-    }
+    console.log('Place:',coordinates.location)
+    return console.log(data.description,'. It is currently '+data.temp,'C','Todays max: '+data.maxtemp,'and Todays min: '+data.mintemp)
   })
-  }
+  
 })
 
